94a4b9a189fe817acc55eda0c0a2513941876f02
angelor88 <angelorussoniello88@gmail.com>
2018-03-08 12:19:41 -0800

    Folder and directories.

---
+++ HairSalon.Tests/ModelTests/HairSalonTests.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HairSalon.Models;
+
+    namespace HairSalonTests
+    {
+        
+    }
